f734ab542007ccd359b9030694bb50c396bb7ca6
Tmeng <tonymengt@gmail.com>
2022-09-02 17:38:28 -0400

    added and tested getHealth, isAlive, and reduceHealth methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,5 +31,30 @@ test('gets inventory from player or returns false', () => {
     expect(player.getInventory()).toEqual(false);
 })
 
+test("get player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+})
+
+test("check if player is alive or not", () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+})
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth-5);
+
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+})
+
 jest.mock('../lib/Potion');
 console.log(new Player('Dave'))
